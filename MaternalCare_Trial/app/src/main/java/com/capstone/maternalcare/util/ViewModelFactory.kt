65d71c31a0ae8ccaffd47a29876709c96f6bb1ff
Saraswati <a180b4kx4070@bangkit.academy>
2024-12-07 17:05:07 +0700

    Update ViewModelFactory.kt
    menambah forgot password dan menghapus // pada profil

--- MaternalCare_Trial/app/src/main/java/com/capstone/maternalcare/util/ViewModelFactory.kt
+++ MaternalCare_Trial/app/src/main/java/com/capstone/maternalcare/util/ViewModelFactory.kt
@@ -4,8 +4,10 @@ package com.capstone.maternalcare.util
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
 import com.capstone.maternalcare.data.model.UserPreference
+import com.capstone.maternalcare.ui.forgot.ForgotPasswordViewModel
 import com.capstone.maternalcare.ui.history.HistoryViewModel
 import com.capstone.maternalcare.ui.login.LoginViewModel
+import com.capstone.maternalcare.ui.profil.ProfilViewModel
 import com.capstone.maternalcare.ui.recomendations.ArticlesViewModel
 import com.capstone.maternalcare.ui.register.RegisterViewModel
 import com.capstone.maternalcare.ui.register.RegisterViewModel2
@@ -28,23 +30,26 @@ class ViewModelFactory(private val pref: UserPreference) :
                 LoginViewModel(pref) as T
             }
 
+            modelClass.isAssignableFrom(ForgotPasswordViewModel::class.java) -> {
+                ForgotPasswordViewModel() as T
+            }
 
             modelClass.isAssignableFrom(RegisterViewModel::class.java) -> {
                 RegisterViewModel(pref) as T
             }
 
-            //modelClass.isAssignableFrom(HomeViewModel::class.java) -> {
-                //HomeViewModel(pref) as T
-            //}
+//            modelClass.isAssignableFrom(HomeViewModel::class.java) -> {
+//                HomeViewModel(pref) as T
+//            }
 
 
             modelClass.isAssignableFrom(ArticlesViewModel::class.java) -> {
                 ArticlesViewModel() as T
             }
 
-            //modelClass.isAssignableFrom(ProfileViewModel::class.java) -> {
-                //ProfileViewModel(pref) as T
-            //}
+            modelClass.isAssignableFrom(ProfilViewModel::class.java) -> {
+                ProfilViewModel(pref) as T
+            }
 
             //modelClass.isAssignableFrom(EditProfileViewModel::class.java) -> {
                 //EditProfileViewModel(pref) as T
@@ -57,6 +62,8 @@ class ViewModelFactory(private val pref: UserPreference) :
             modelClass.isAssignableFrom(RegisterViewModel2::class.java) -> {
                 RegisterViewModel2(pref) as T
             }
+
+
 
             else -> throw IllegalArgumentException("Unknown ViewModel class: " + modelClass.name)
         }
